c6327925522640b46737ee6b3c2b976f836de0fb
Somayeh.Ghanavati <s.ghanavati94@gmail.com>
2022-11-30 18:44:52 +0330

    Add put

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
+const { request, response } = require('express')
 const express = require('express')
+const Joi = require('joi')
 const app = express()
 app.use(express.json())
 const courses = [
@@ -6,27 +8,35 @@ const courses = [
   { id: 2, name: 'course2' },
   { id: 3, name: 'course3' },
 ]
+
 app.get('/', (req, res) => res.send('Hello 10  Word'))
-/* app.get('/api/courese', (req, res) => {
-  res.send([1, 2, 3])
-}) */
-app.get('/api/courese/:id', (request, response) => {
+app.get('/api/courses', (req, res) => {
+  res.send(courses)
+})
+app.get('/api/courses/:id', (request, response) => {
   response.send(request.params.id)
 })
+
 app.get('/api/posts/:year/:month', (request, response) => {
   /*  response.send(request.params) */
   response.send(request.query) //http://localhost:3000/api/posts/1/2?sortBy=name
 })
+
 app.get('/api/courses/:id', (request, response) => {
   const course = courses.find((c) => c.id === parseInt(request.params.id))
   if (!course)
     response.status(404).send('The course with the given ID was not found')
   response.send(course)
 })
+
 app.post('/api/courses', (request, response) => {
-  if (!request.body.name || request.body.name.length < 3)
-    response.status(400).send('Name is required and should be minimumm 3 char')
-  return
+  //validation
+  const { error } = validateCoures(request.body)
+
+  if (error) {
+    response.status(400).send(error.details[0].message)
+    return
+  }
   const course = {
     id: courses.length + 1,
     name: request.body.name,
@@ -35,5 +45,40 @@ app.post('/api/courses', (request, response) => {
   response.send(courses)
 })
 
+//update
+app.put('/api/courses/:id', (request, response) => {
+  //Look up the course  //If not existing ,return 404
+
+  const course = courses.find((x) => x.id === parseInt(request.params.id))
+  if (!course) {
+    response.status(404).send('The course with the given ID was not found')
+    return
+  }
+  //validate //If invalid ,return 400 -Bad request
+
+  const { error } = validateCoures(request.body)
+  console.log(error)
+  if (error) {
+    response.status(400).send(error.details[0].message)
+    return
+  }
+
+  //Update course
+
+  course.name = request.body.name
+  response.send(course)
+  //Return the updated course
+})
+
 var port = process.env.PORT || '3000'
 app.listen(port, () => console.log(`server is running on ${port}`))
+
+function validateCoures(course) {
+  debugger
+  console.log(course)
+  const schema = Joi.object({
+    name: Joi.string().min(3).required(),
+  })
+
+  return schema.validate(course)
+}
